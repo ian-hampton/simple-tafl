a39332e7909a3ed1acfd9c4d649f3922469e4362
ian-hampton <ian@ianhampton.com>
2026-08-09 13:36:51 -0400

    seperated core logic from api calls using services file

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,53 +1,25 @@
 from flask import Blueprint, request, abort
 
-from app.core import board
-from app.core import state
-from app.core import validation
-from app.core import move
+from app.services import services
 
 api_bp = Blueprint("api", __name__)
 
 @api_bp.route("/state", methods=["GET"])
 def game_state():
-    data = {
-        "board": board.get_board_state(),
-        "turn": state.get_game_state()
-    }
-    return data
+    return services.get_game_state()
 
 @api_bp.route("/moves", methods=["POST"])
 def moves():
     data = request.get_json()
     location = data.get("move")
-
-    id = board.location_to_id(location)
-    legal_moves = validation.get_legal_moves(id)
-
-    legal_moves_final = []
-    for id in legal_moves:
-        legal_moves_final.append(board.id_to_location(id))
-
-    data = {
-        "moves": legal_moves_final
-    }
-    
-    return data
+    return services.get_legal_moves(location)
 
 @api_bp.route("/move", methods=["POST"])
 def resolve_move():
     data = request.get_json()
     start = data.get("start")
     dest = data.get("end")
-
-    start_id = board.location_to_id(start)
-    dest_id = board.location_to_id(dest)
-
-    # reject move if it is invalid - should not occur just in case
-    legal_moves = validation.get_legal_moves(start_id)
-    if dest_id not in legal_moves:
+    success = services.execute_move(start, dest)
+    if not success:
         abort(422)
-
-    # resolve move
-    move.resolve(start_id, dest_id)
-
     return {}, 200
---
+++ backend/app/services/services.py
@@ -0,0 +1,65 @@
+from app.core import board, move, state, validation
+
+def get_game_state() -> dict:
+    """
+    Fetches all game state information.
+
+    Returns:
+        dict: JSON-ready dictionary containing board state and game turn.
+    """
+    data = {
+        "board": board.get_board_state(),
+        "turn": state.get_game_state()
+    }
+    return data
+
+def get_legal_moves(location: str) -> dict:
+    """
+    Fetches all the legal moves for a piece at a specific location.
+
+    Args:
+        location (str): Coordinate pair.
+
+    Returns:
+        dict: JSON-ready dictionary containing list of legal destinations.
+    """
+    id = board.location_to_id(location)
+    legal_moves = validation.get_legal_moves(id)
+
+    legal_moves_final = []
+    for id in legal_moves:
+        legal_moves_final.append(board.id_to_location(id))
+
+    data = {
+        "moves": legal_moves_final
+    }
+    
+    return data
+
+def execute_move(start: str, dest: str) -> bool:
+    """
+    Attempts to move piece in start coordinate to destination coordinate.
+
+    All moves that come in should have already been validated by a previous request.
+
+    Just in case, we will verify by running the legal moves service again.
+
+    Args:
+        start (str): Coordinate pair.
+        dest (str): Coordinate pair.
+
+    Returns:
+        bool: True if move successful, False otherwise.
+    """
+    start_id = board.location_to_id(start)
+    dest_id = board.location_to_id(dest)
+
+    # reject move if it is invalid - should not occur just in case
+    legal_moves = validation.get_legal_moves(start_id)
+    if dest_id not in legal_moves:
+        return False
+
+    # resolve move
+    move.resolve(start_id, dest_id)
+
+    return True
