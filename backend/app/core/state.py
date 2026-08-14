760cbf9b659fcdf9acb94b54d46dbd7cdb4074bd
ian-hampton <ian@ianhampton.com>
2026-08-14 13:06:18 -0400

    encirclement victory done

--- backend/app/core/state.py
+++ backend/app/core/state.py
@@ -1,4 +1,6 @@
+import heapq
 from enum import Enum
+from typing import List, Tuple
 
 from app.core import board
 
@@ -20,6 +22,58 @@ def white_victory() -> bool:
 def black_victory_king() -> bool:
     return not 'K' in board.get_board_state()
 
+def black_victory_encirclement() -> bool:
+    """
+    Using flood fill algorithm to test for encirclement.
+
+    Returns:
+        bool: True if white pieces are all encircled, false otherwise.
+    """
+    heap: List[Tuple[int, str]] = []
+    visited = set()
+    floodfill = ['0'] * 121
+
+    # flood fill from all white pieces
+    for index, char in enumerate(board.get_board_state()):
+        tile_id = str(index)
+        if not board.is_white(tile_id):
+            continue
+        heapq.heappush(heap, (0, tile_id))
+
+    # dfs
+    while heap:
+        priority, tile_id = heapq.heappop(heap)
+
+        visited.add(tile_id)
+        floodfill[int(tile_id)] = '1'
+
+        adjacent = board.get_adjacent(tile_id)
+        up = adjacent[0]
+        down = adjacent[1]
+        left = adjacent[2]
+        right = adjacent[3]
+
+        if up[0] != 'N/A' and up[0] not in visited and not board.is_black(up[0]):
+            heapq.heappush(heap, (priority + 1, up[0]))
+
+        if down[0] != 'N/A' and down[0] not in visited and not board.is_black(down[0]):
+            heapq.heappush(heap, (priority + 1, down[0]))
+
+        if left[0] != 'N/A' and left[0] not in visited and not board.is_black(left[0]):
+            heapq.heappush(heap, (priority + 1, left[0]))
+
+        if right[0] != 'N/A' and right[0] not in visited and not board.is_black(right[0]):
+            heapq.heappush(heap, (priority + 1, right[0]))
+
+    # check edges for fill
+    for index, char in enumerate(floodfill):
+        if char != '1':
+            continue
+        if board.is_on_edge(index):
+            return False
+
+    return True
+
 def set_game_state() -> None:
     global STATE
 
@@ -31,7 +85,9 @@ def set_game_state() -> None:
         STATE = GameState.WHITE_WIN
         return
 
-    # TODO - check if Black has won via encirclement
+    if black_victory_encirclement():
+        STATE = GameState.BLACK_WIN
+        return
 
     # otherwise, move to next turn
     if STATE == GameState.BLACK_TURN:
